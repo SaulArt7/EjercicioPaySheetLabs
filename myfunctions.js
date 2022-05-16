8fa5ca90478b9ec3a6d3f455ef41ee99ff2567ff
SaulArt7 <saul.rivera@uptc.edu.co>
2022-05-16 17:36:15 -0500

    Commit TerceraPrueba de Modificación

---
+++ myfunctions.js
@@ -0,0 +1,11 @@
+function go(){
+
+  if (document.getElementById("exampleInputPassword1").value=='123'){ 
+        window.open("registro.html");
+    } 
+    else{ 
+         alert("Porfavor ingrese, nombre de usuario y contraseña correctos. Intenta con tu nombre y con '123' como contraseña"); 
+    } 
+} 
+
+     
